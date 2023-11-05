f646726ec2babf54f8f81645b7f365b6b2baa5be
Hnat Liashenko <hnat.liashenko@bluepes.com>
2023-11-05 16:00:40 +0200

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 "use client";
 import { FC } from "react";
-import { Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 
 import Protected from "@/components/HOC/Protected";
 
 const Home: FC = () => {
-  return <Container component="section">CourseWork Front End</Container>;
+  return (
+    <Container component="header">
+      <Typography variant="h4" component="h1" mt={10}>
+        ПЗ для обліку виконання робіт на посту обслуговування станції
+        автосервісу
+      </Typography>
+      <Typography variant="h5" component="h2" mt={4}>
+        Скористайтесь навігаційною панеллю вгорі для переходу між сторінками
+      </Typography>
+    </Container>
+  );
 };
 
 export default Protected(Home);
