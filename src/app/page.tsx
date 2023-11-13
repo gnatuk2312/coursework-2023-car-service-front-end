94791a90ce6c4d7ec18a57e0e64820d5a693ca42
Hnat Liashenko <hnat.liashenko@bluepes.com>
2023-11-13 19:57:24 +0200

    create todo list on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,23 @@ const Home: FC = () => {
       <Typography variant="h5" component="h2" mt={4}>
         Скористайтесь навігаційною панеллю вгорі для переходу між сторінками
       </Typography>
+      <Typography variant="h6" component="h3" mt={4}>
+        {`TODO: improve Auth layer (update admin session on page reload)`}
+        <br />
+        {`TODO: create responsive layout for mobile and tablet`}
+        <br />
+        {`TODO: put all business logic into local hooks`}
+        <br />
+        {`TODO: make spare parts to be optional on creating performed work`}
+        <br />
+        {`TODO: update <NoData/> components image`}
+        <br />
+        {`TODO: create reusable <HeaderSection/> component`}
+        <br />
+        {`TODO: add pending state for all dialog actions`}
+        <br />
+        {`TODO: handle errors in dialog actions`}
+      </Typography>
     </Container>
   );
 };
